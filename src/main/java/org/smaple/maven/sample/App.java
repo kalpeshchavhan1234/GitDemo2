6e3efdcd30cf4f8ce26d9e92f7e814534b088f88
kalpesh <kalpeshchavhan1234@gmail.com>
2025-09-07 14:12:45 +0530

    New commit message

---
+++ src/main/java/org/smaple/maven/sample/App.java
@@ -0,0 +1,13 @@
+package org.smaple.maven.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
